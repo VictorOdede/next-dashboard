1f924702d11e543a156a91191d15c2812445b479
vico <victorch9@gmail.com>
2023-04-05 20:18:49 +0300

    added header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-gray-200 min-h-screen"></main>
+      <main className="bg-gray-200 min-h-screen">
+        <Header />
+        <Sidebar />
+      </main>
     </>
   );
 }

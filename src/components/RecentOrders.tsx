c5d9d3589adb752ea0b04c6b6c1a119b876f2de4
vico <victorch9@gmail.com>
2023-04-11 20:43:47 +0300

    recent orders tab added

--- src/components/RecentOrders.tsx
+++ src/components/RecentOrders.tsx
@@ -1,7 +1,65 @@
+import { faker } from "@faker-js/faker";
+import { RxClipboard } from "react-icons/rx";
+
+interface OrderData {
+  id: number;
+  name: {
+    first: string;
+    last: string;
+  };
+  total: string;
+  status: string;
+  method: string;
+  date: string;
+}
+
 export const RecentOrders = () => {
+  const data: OrderData[] = [];
+
+  for (let i = 1; i <= 10; i++) {
+    let item = {
+      id: i,
+      name: {
+        first: faker.name.firstName(),
+        last: faker.name.lastName(),
+      },
+      total: faker.finance.amount(50, 300, 2, "$"),
+      status: faker.helpers.arrayElement([
+        "On hold",
+        "Processing",
+        "Completed",
+      ]),
+      method: faker.finance.creditCardIssuer(),
+      date: `${faker.datatype.number({ min: 2, max: 59 })}minutes ago`,
+    };
+    data.push(item);
+  }
+
   return (
-    <div>
-      <p>Recent Orders</p>
+    <div className="w-full col-span-1 relative lg:h-[70vh] h-[50-vh] m-auto p-4 border rounded-lg bg-white overflow-scroll shadow-md">
+      <h1>Recent Orders</h1>
+      <ul>
+        <li>
+          {data.map((order, id) => (
+            <li
+              key={id}
+              className="bg-gray-50 hover:bg-gray-100 rounded-lg  my-3 p-2 flex items-center cursor-pointer"
+            >
+              <div className="bg-blue-200 rounded-lg p-3">
+                <RxClipboard size={20} color="#3B82F6" />
+              </div>
+              <div className="pl-4">
+                <p className="text-gray-800 font-bold">{order.total}</p>
+                <p className="text-gray-400 text-sm">{`${order.name.first} ${order.name.last}`}</p>
+                <p></p>
+              </div>
+              <p className=" lg:flex md:hidden absolute right-6 text-sm text-gray-500">
+                {order.date}
+              </p>
+            </li>
+          ))}
+        </li>
+      </ul>
     </div>
   );
 };
